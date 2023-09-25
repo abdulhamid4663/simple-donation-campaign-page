6d8788087bc3b2815b2fc5bb2637ea7b499e49bb
abdulhamid4663 <abdulhamid4663@gmail.com>
2023-09-26 04:53:57 +0600

    added custom Tooltip showing percentage onHover

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -20,25 +20,49 @@ const Statistics = () => {
 
     }, [allData])
 
+    function getPercentage(num1, num2) {
+        return (num1 - num2) / (num1) * 100
+    }
+
+    const totalDonationPercentage = getPercentage(allData.length, donatedData.length);
+    const myDonationPercentage = getPercentage(allData.length, newTotalData.length);
+
+    const totalDonationInt = Number.parseFloat(totalDonationPercentage.toFixed(2));
+    const myDonationInt = Number.parseFloat(myDonationPercentage.toFixed(2));
+
     const data01 = [
         {
-            "name": "Total Donation",
-            "value": newTotalData.length,
-            "fill": "red",
+            name: `Total Donation`,
+            value: totalDonationInt,
+            fill: "red",
         },
         {
-            "name": "My Donation",
-            "value": donatedData.length,
-            "fill": "green",
+            name: `My Donation`,
+            value: myDonationInt,
+            fill: "green",
         },
     ];
+
+    const customTooltip = ({active, payload}) => {
+        if(active) {
+            const data = payload[0].payload;
+
+            return (
+                <div className="py-2 px-6 bg-[#ffffffcc]">
+                    <p className="font-extrabold">{`${data.name} : ${(data.value)}%`}</p>                        
+                </div>
+            );
+        }
+
+        return null
+    }
 
     return (
         <ResponsiveContainer width="100%" height={700}>
             <PieChart>
-                <Pie data={data01} dataKey="value" cx="50%" cy="50%" outerRadius={165} />
-                <Legend iconType="plainline" iconSize={40}/>
-                <Tooltip />
+                <Pie data={data01} dataKey={`value`} text cx="50%" label cy="50%" outerRadius={165} />
+                <Legend iconType="plainline" iconSize={40} />
+                <Tooltip content={customTooltip} cursor="pointer"/>
             </PieChart>
         </ResponsiveContainer>
     );
